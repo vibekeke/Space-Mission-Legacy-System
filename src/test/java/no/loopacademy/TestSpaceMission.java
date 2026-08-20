89c13918e46b31231920ba5bbfb49103a47a3c61
Vibeke <vise248@gmail.com>
2026-08-20 09:24:39 +0200

    add: testRunMission

---
+++ src/test/java/no/loopacademy/TestSpaceMission.java
@@ -0,0 +1,106 @@
+package no.loopacademy;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestSpaceMission {
+
+    @Test
+    public void testRunMission_telemetryReadingTypeTemp_hasCorrectStringOutput () {
+        //ARRANGE
+        Writer writer = new MockWriter();
+        TelemetryReading reading = MockTelemetryReading.Builder()
+                                        .withType("TEMP")
+                                        .build();
+        TelemetryClient client = new MockTelemetryClient(reading);
+
+        SpaceMission mission = new SpaceMission() {
+            @Override
+            protected Writer createWriter() throws IOException {
+                return writer;
+            }
+
+            @Override
+            protected TelemetryClient createTelemetryClient() {
+                return client;
+            }
+        };
+
+        String expectedLine = "Temperature: " + reading.value + " C at " + reading.timestamp + "\n";
+        String expectedOutput = expectedLine.repeat(5);
+
+        //ACT
+        mission.runMission();
+        MockWriter output = (MockWriter)writer;
+        String actualOutput = (output.getContent());
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+        @Test
+    public void testRunMission_telemetryReadingTypePressure_hasCorrectStringOutput () {
+        //ARRANGE
+        Writer writer = new MockWriter();
+        TelemetryReading reading = MockTelemetryReading.Builder()
+                                        .withType("PRESSURE")
+                                        .build();
+        TelemetryClient client = new MockTelemetryClient(reading);
+
+        SpaceMission mission = new SpaceMission() {
+            @Override
+            protected Writer createWriter() throws IOException {
+                return writer;
+            }
+
+            @Override
+            protected TelemetryClient createTelemetryClient() {
+                return client;
+            }
+        };
+
+        String expectedLine = "Pressure: " + reading.value + " kPa at " + reading.timestamp + "\n";
+        String expectedOutput = expectedLine.repeat(5);
+
+        //ACT
+        mission.runMission();
+        MockWriter output = (MockWriter)writer;
+        String actualOutput = (output.getContent());
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testRunMission_telemetryReadingTypeOxygen_hasCorrectStringOutput () {
+        //ARRANGE
+        Writer writer = new MockWriter();
+        TelemetryReading reading = MockTelemetryReading.Builder()
+                                        .withType("OXYGEN")
+                                        .build();
+        TelemetryClient client = new MockTelemetryClient(reading);
+
+        SpaceMission mission = new SpaceMission() {
+            @Override
+            protected Writer createWriter() throws IOException {
+                return writer;
+            }
+
+            @Override
+            protected TelemetryClient createTelemetryClient() {
+                return client;
+            }
+        };
+
+        String expectedLine = "Oxygen: " + reading.value + " % at " + reading.timestamp + "\n";
+        String expectedOutput = expectedLine.repeat(5);
+
+        //ACT
+        mission.runMission();
+        MockWriter output = (MockWriter)writer;
+        String actualOutput = (output.getContent());
+
+        assertEquals(expectedOutput, actualOutput);
+    }
+}
