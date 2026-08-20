937e96de2da14c2b959d823ec0d117d113466e73
Vibeke <vise248@gmail.com>
2026-08-20 09:24:05 +0200

    create seam-able methods

--- src/main/java/no/loopacademy/SpaceMission.java
+++ src/main/java/no/loopacademy/SpaceMission.java
@@ -2,13 +2,18 @@ package no.loopacademy;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 import java.time.LocalDateTime;
 
 public class SpaceMission {
-    private TelemetryClient client = new TelemetryClient();
+    private TelemetryClient client;
+
+    public SpaceMission() {
+        this.client = createTelemetryClient();
+    }
 
     public void runMission() {
-        try (FileWriter fw = new FileWriter(Config.logFile, true)) {
+        try (Writer fw = createWriter()) {
             for (int i = 0; i < 5; i++) {
                 TelemetryReading r = client.read();
 
@@ -27,8 +32,16 @@ public class SpaceMission {
         }
     }
 
+    protected Writer createWriter() throws IOException {
+        return new FileWriter(Config.logFile, true);
+    }
+
+    protected TelemetryClient createTelemetryClient() {
+        return new TelemetryClient();
+    }
+
     public void emergencyLog(String message) {
-        try (FileWriter fw = new FileWriter(Config.logFile, true)) {
+        try (Writer fw = createWriter()) {
             fw.write("EMERGENCY: " + message + " at " + LocalDateTime.now() + "\n");
         } catch (IOException e) {
             e.printStackTrace();
