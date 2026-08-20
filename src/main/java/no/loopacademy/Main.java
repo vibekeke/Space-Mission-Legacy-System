6e717220669c6825f51555f3d67fd98dc0b8733d
Vibeke <vise248@gmail.com>
2026-08-20 09:24:47 +0200

    nit: remove space

--- src/main/java/no/loopacademy/Main.java
+++ src/main/java/no/loopacademy/Main.java
@@ -3,16 +3,11 @@ package no.loopacademy;
 public class Main {
     public static void main(String[] args) {
 
-
-
-
         SpaceMission mission = new SpaceMission();
-        mission.runMission();
+        for (int i = 0; i < 10; i++) {
+            mission.runMission();
+        }
         mission.emergencyLog("oxygen leak detected");
-
-
-
-
-
+    
     }
 }
